cd7493f197792844fc9f8d2bbeb24e2c61c5fe8f
Abbey Mondshein <abbey.sync@gmail.com>
2018-08-22 23:49:44 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,9 @@
+var recipes = {}
+
+//Objects updateObjectWithKeyAndValue(object, key, value) returns an object with the orignal key value pairs and the new key value pair:
+//it does not modifythe original object, but rather returns a clone with the new data
+function updateObjectWithKeyAndValue(object,key,value) {
+  object[key] = value
+  return recipes
+  }
+  
