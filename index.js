e4193dfc0dc3f9cefc129143642235853ecef8aa
Abbey Mondshein <abbey.sync@gmail.com>
2018-08-22 23:50:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,4 +6,6 @@ function updateObjectWithKeyAndValue(object,key,value) {
   object[key] = value
   return recipes
   }
-  
+function updateObjectWithKeyAndValue(object, key, value) {
+ return Object.assign({}, obj, { [key]: value })
+}
