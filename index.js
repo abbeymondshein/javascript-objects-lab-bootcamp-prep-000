5791a0c337316da4a90a90a348986c6659410050
Abbey Mondshein <abbey.sync@gmail.com>
2018-08-23 00:00:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,5 +15,5 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 
 //Objects deleteFromObjectByKey(object, key) deletes `key` from a clone of object and returns the new object (it is non-destructive):
 function deleteFromObjectByKey(object, key) {
-  
+  return Object.assign({}, object, { [key]: value })
 }
